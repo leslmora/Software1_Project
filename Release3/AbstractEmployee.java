d7eed40b1e40418c54b5215c216925a56f5775f7
leslmora <36290621+leslmora@users.noreply.github.com>
2020-05-01 14:11:02 -0500

    Create AbstractEmployee.java

---
+++ Release3/AbstractEmployee.java
@@ -0,0 +1,6 @@
+//variables related to the employee Null Pattern
+public abstract class AbstractEmployee {
+    public String name;
+    public abstract boolean NotName();
+    public abstract String getName();
+}
